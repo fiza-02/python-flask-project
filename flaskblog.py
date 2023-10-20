a6456542e5db970d618bf71efb1c28563371967b
Mohammed Yasmin <mohammed.yasmin@hcl.com>
2023-10-20 18:40:01 +0530

    added database structure using sql-alchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 
 '''
 >>> import secrets
@@ -12,6 +15,37 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fac072214929018288a5d4c197bbf3f0'
 # use random set of characters for secret key
 # dummy data for rendering posts
+
+# DATABASE_URL = 'mssql+pyodbc://myuser:mypassword@myserver/mydatabase?driver=ODBC+Driver+17+for+SQL+Server'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://sa:Viswafiza%4002@WIN-CFN9TN3HFBQ/dummy?driver=ODBC+Driver+17+for+SQL+Server"
+
+db = SQLAlchemy(app)
+print('db connected')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}',' {self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}',' {self.content}')"
+
 
 posts = [
     {
@@ -41,15 +75,16 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/register",methods=['GET','POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
-    return render_template('register.html',title='Register', form=form)
-    
-@app.route("/login",methods=['GET','POST'])
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -58,7 +93,8 @@ def login():
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html',title='=Login', form=form)
+    return render_template('login.html', title='=Login', form=form)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
