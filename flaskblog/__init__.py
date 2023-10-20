a15f30b1da50def2258eefe02bd60b737d53e8b1
Mohammed Yasmin <mohammed.yasmin@hcl.com>
2023-10-20 19:48:39 +0530

    structuring into package

---
+++ flaskblog/__init__.py
@@ -0,0 +1,24 @@
+#__init__.py where we initialize our application adn bring together differnet components
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+'''
+>>> import secrets
+>>> secrets.token_hex(16)
+'f8d1e7b246c52fd81b103f0e265294ca'
+'''
+# when we use these forms we need a secret key , a secret key will protect against modifiying cookies and cross-site request forgery attacks
+app.config['SECRET_KEY'] = 'fac072214929018288a5d4c197bbf3f0'
+# use random set of characters for secret key
+# dummy data for rendering posts
+
+# DATABASE_URL = 'mssql+pyodbc://myuser:mypassword@myserver/mydatabase?driver=ODBC+Driver+17+for+SQL+Server'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://sa:Viswafiza%4002@WIN-CFN9TN3HFBQ/dummy?driver=ODBC+Driver+17+for+SQL+Server"
+
+db = SQLAlchemy(app)
+print('db connected')
+
+from flaskblog import routes
